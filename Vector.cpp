f9d5e80312c4276ca9c9d4bf3bcf56813cefd5e0
Kirill Brotskiy <56027225+kbrotskiy@users.noreply.github.com>
2020-03-07 10:39:34 +0300

    Update Vector.cpp

--- Vector.cpp
+++ Vector.cpp
@@ -5,96 +5,69 @@
 #include <algorithm> 
 
 
-Vector::Vector(int *p, int n) {
-	this->n = n;
-	this->p = new int[n];
-	for (int i = 0; i < n; i++) this->p[i] = p[i];
-}
+    Vector::Vector(){
+        pv=nullptr;
+        k=0;
+    }
+    
+    Vector::Vector(double *ws, int n){
+        this->k=n;
+        this->pv=new double[n];
+        for (int i=0;i<n;i++) this->pv[i]=ws[i];
+    }
+    
+    Vector::Vector(const Vector & V){
+        k=V.k;
+        pv=new double[k];
+        for (int i=0;i<k;i++) pv[i]=V.pv[i];
+    }
+    
+    Vector::Vector(Vector&& V){
+        V.pv=nullptr;
+        V.k=0;
+    }
+    
+    Vector::~Vector(){
+        if (pv!=nullptr) delete[]pv;
+    }
+    
+    double & operator[](int index){
+        return pv[index];
+    }
+    
+    Vector & operator=(Vector& vec2){
+        k=vec2.k;
+        if (pv!=nullptr) delete[] pv;
+        pv=new double[k];
+        for (int i=0;i<k;i++){
+            pv[i]=vec2.pv[i];
+        }
+        return *this;
+    }
+    friend double *operator +(Vector& V1,   double *v2);
+    friend istream& operator>>(istream& in, Vector& vector);
+};
 
-Vector::Vector() {
-	p = nullptr;
-	n = 0;
-}
-
-Vector::Vector(const Vector & V)
+double *operator +(Vector& V1,   double *v2)
 {
-	cout << "Copy constructor" << endl;
-	n = V.n;
-	p = new int[n];
-	for (int i = 0; i < n; i++)
-		p[i] = V.p[i];
+    double *p = new double[V1.k];
+    for (int i = 0; i<V1.k; i++)
+        p[i] = V1.pv[i] + v2[i];
+    return p;
 }
 
-Vector::Vector(Vector&& V)
-	: p(V.p), n(V.n)
-{
-	cout << "Move constructor" << endl;
-	V.p = nullptr;
-	V.n = 0;
+istream& operator>>(istream& in, Vector& vector) {
+    vector.pv=new double[5];
+    vector.k = 5;
+    for (int i = 0; i<5; i++) {
+        in >> vector[i];
+    }
+    return in;
 }
 
-Vector::~Vector() // Деструктор
-{
-	if (p != nullptr) delete[] p; // Освобождаем память
-}
-
-Vector& Vector::operator^(const Vector & vector) {
-	int * v = new int[vector.n];
-	for (int i = 0; i < vector.n; i++) {
-		v[i] = p[i] ^ vector.p[i];
-	}
-	Vector * result = new Vector(v, vector.n);
-	delete[] v;
-	return *result;
-}
-
-
-Vector& Vector::operator=(const Vector& v2) // Оператор- функция копирования объекта 
-{
-	cout << "Assignment copy operator" << endl;
-	n = v2.n;
-	if (p != nullptr) delete[] p; // Освобождаем память старого вектора
-	p = new int[n]; // Выделяем память для нового вектора
-	for (int i = 0; i < n; i++) p[i] = v2.p[i];
-	return *this; // Возвращаем ссылку на текущий объект
-}
-
-Vector& Vector::operator=(Vector&& v2) // Оператор- функция копирования объекта 
-{
-	cout << "Assignment move operator" << endl;
-	n = v2.n;
-	if (p != nullptr) delete[] p; // Освобождаем память старого вектора
-	p = new int[n]; // Выделяем память для нового вектора
-	for (int i = 0; i < n; i++) p[i] = v2.p[i];
-	v2.p = nullptr;
-	v2.n = 0;
-	return *this; // Возвращаем ссылку на текущий объект
-}
-
-
-istream & operator >> (istream& in, Vector & v) {
-	string number;
-	vector<int> vector;
-	while (in >> number)
-	{
-		vector.push_back(std::stod(number));
-	}
-
-	v.p = new int[vector.size()];
-	for (int i = 0; i < vector.size(); i++) {
-		v.p[i] = vector[i];
-	}
-	v.n = vector.size();
-	return in;
-}
-
-ostream & operator << (ostream& out, Vector & vector) {
-	for (int i = 0; i < vector.n; i++) {
-		out << vector.p[i];
-		if (i != vector.n - 1) {
-			out << ' ';
-		}
-	}
-	cout << endl;
-	return out;
+ofstream& operator<<(ofstream& out, Vector& vector) {
+    for (int i = 0; i<5; i++) {
+        out << vector[i];
+    }
+    return out;
 }

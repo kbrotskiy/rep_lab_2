cfa5fdf85a3c586e487f2dce96dbb5b46e8ff4ac
Kirill Brotskiy <56027225+kbrotskiy@users.noreply.github.com>
2020-03-07 08:40:00 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,11 +7,16 @@
 //
 
 #include <iostream>
+#include <fstream>
 #include <locale.h>
 #define pr print();
- 
- 
-class Vector_1
+using namespace std;
+ double w1[5];
+ double w2[5];
+ int i;
+ string str;
+
+class Vector
 {
     int k;
     double *pv;
@@ -21,13 +26,14 @@ public:
             return pv[index];
         }
     
-    Vector_1()
-        {
-            pv = 0;
-            k = 0;
-        }
+//    Vector()
+//        {
+//            pv = 0;
+//            k = 0;
+//        }
  
-    Vector_1(double *ws, int n)
+    
+    Vector(double *ws, int n)
         {
             this->k = n;
             this->pv = new double[n];
@@ -35,7 +41,37 @@ public:
             this->pv[i] = ws[i];
         }
     
-    Vector_1 & operator =(Vector_1& vec_2)
+    Vector(const Vector & V)
+    { // Конструктор копирования
+        k = V.k;
+        pv = new double[k];
+        for (int i = 0; i < k; i++)
+            pv[i] = V.pv[i];
+    }
+
+    // Конструктор перемещения
+    
+    Vector(Vector&& V)  // Параметр - правосторонняя ссылка
+        : pv(V.pv), k(V.k)   // Можно инициализировать так
+    {
+        //p = M.p;  // Или инициализировать так
+        //n = M.n;
+        // Присвойте данным-членам исходного объекта значения по умолчанию. Это не позволяет деструктору освобождать память
+        V.pv = nullptr;
+        V.k = 0;
+    }
+    
+    
+    Vector() {
+    pv = nullptr; k = 0; // Конструктор без параметров, задает "пустой" объект
+    }
+    double &operator[](int index) // Оператор - функция (перегрузка операции обращения к элементу)
+    {
+    return pv[index];
+    }
+    
+    
+    Vector & operator =(Vector& vec_2)
         {
             k = vec_2.k;
             pv = new double[k];
@@ -43,45 +79,70 @@ public:
                 return *this;
         }
  
-    void print()
+    void print() const // Печать + заменa на перегрузку
         {
-            printf("\n");
-            for (int i = 0; i<k; i++)
-                printf(" %.2lf ", pv[i]);
+            for (int i = 0; i < k; i++)
+                std::cout << pv[i] << " ";
+            std::cout << std::endl;
         }
  
-    ~Vector_1()
+    ~Vector()
         {
             if (k>0) delete[] pv;
         }
-    friend double *operator +(Vector_1& V1,   double *v2);
+    friend double *operator +(Vector& V1,   double *v2);
 };
  
-double *operator +(Vector_1& V1,   double *v2)
+double *operator +(Vector& V1,   double *v2)
     {
         double *p = new double[V1.k];
         for (int i = 0; i<V1.k; i++)
             p[i] = V1.pv[i] + v2[i];
         return p;
     }
- 
+
+void input(){
+    ifstream file("input.txt");
+    for (i=0;i<5;i++)
+    {
+        file>>w1[i];
+    }
+    getline(file, str);
+    for (i=0;i<5;i++)
+    {
+        file>>w2[i];
+    }
+    file.close();
+}
+
+void output(){
+    ifstream file("output.txt");
+    V1.pr;
+    V2.pr;
+    for (i=0;i<5;i++)
+    {
+        fout<<V1[i];
+    }
+    V3.pr;
+    file.close();
+}
 int main() {
     setlocale(LC_ALL, "rus");
-    double w1[] = { 1.4, 3.2, 4.7, 6.5, 8, 20 };
-    Vector_1 V1(w1, 6); // Создаем объект
-    double w2[] = { 9, 3.1, 5, 6.6, 14.7, 18 };
-    Vector_1 V2(w2, 6);
+    input();
+    Vector V1(w1, 5); // Создаем объект
+    Vector V2(w2, 5);
     V1.pr;
     V2.pr;
     printf("\nПерегрузка операции []...\n");
-    for (int i = 0; i<6; i++)
+    for (int i = 0; i<5; i++)
         printf(" %.2f ", V1[i]);
     double *w3;
     printf("\n\nПерегрузка операции: +");
     w3 = V1 + w2;
-    Vector_1 V3(w3, 6);
+    Vector V3(w3, 6);
     printf("\nCуммированный вектор: ");
     V3.pr;
+    output();
     printf("\n");
     return 0;
 }

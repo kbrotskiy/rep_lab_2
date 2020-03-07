a3c170ee342728862dddc53630b963e9801031d3
Kirill Brotskiy <56027225+kbrotskiy@users.noreply.github.com>
2020-03-07 10:54:57 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,27 +6,18 @@
 using namespace std;
 
 int main(){
-//    int n;
     Vector vector1;
     Vector vector2;
     string str;
     ifstream fin("input.txt");
-    double *w1=new double[5];
     if (fin.is_open()) {
         fin >> vector1;
     }
-    double *w2=new double[5];
     getline(fin, str);
     if (fin.is_open()) {
         fin >> vector2;
     }
     fin.close();
-    Vector V1(w1, 5); // Создаем объект
-    Vector V2(w2, 5);
-    double *w3;
-    w3 = V1 + w2;
-    Vector V3(w3, 5);
-
     ofstream fout("Output.txt");
     for (int i=0;i<5;i++){
         fout << V3 << " ";
